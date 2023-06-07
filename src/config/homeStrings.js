bf9d2149835ab8a547a8e1e0aa37c9281024371c
Grant Li <grant@jostle.me>
2023-06-06 23:18:39 -0700

    youtube video embed

--- src/config/homeStrings.js
+++ src/config/homeStrings.js
@@ -1,7 +1,10 @@
 const homeStrings = {
     home: {
         title: "TG DANCESPORT",
-        slogan: "Group classes. Private lessons. Wedding dances. And so much more."
+        slogan: "Group classes. Private lessons. Wedding dances. And so much more.",
+        section1_title: "Happy, Healthy, and Esxtraordinary Life Awaits, Start Dancing!",
+        section1_content: "​Welcome to TGDancesport! Tony and Gloria are internationally trained professional ballroom dance instructors based in Vancouver BC, Canada. You will find all the information about their group classes for beginners, social dancers, and competitors on this website. They offer ballroom dance lessons for everyone starting from age 4 to adults of all ages. Come join the fun and learn how to ballroom dance in a friendly and relaxed environment. Looking for the best ballroom dance instructors who are also world class competitive dancers and experienced teachers? Register for their group classes and learn from Champions who are passionate about teaching you how to dance!",
+        
     },
     about: {
         title: "About Us",
--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import './Home.css';
 import { HeroSection } from '../../component';
+import { homeStrings } from '../../config';
 
 const Home = () => {
     return (
         <div className="home-container">
             <HeroSection />
-            <div>Slogan and simple intro</div>
-            <div>Cards on classes offered</div>
-            <div>What students are saying</div>
-            <div>Gallery</div>
+            <div className="bg-white home-section">
+                <div className="home-title">{homeStrings.home.section1_title}</div>
+                <div className="home-text">{homeStrings.home.section1_content}</div>
+                <div className="home-video-wrapper">
+                    <div className="home-video-container">
+                        <iframe class="home-video" src="https://www.youtube.com/embed/-FbtfDw3oHY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-grey home-section">
+                Cards on classes offered
+            </div>
+            <div className="bg-white home-section">
+                What students are saying
+            </div>
+            <div className="bg-grey home-section">
+                Gallery
+            </div>
         </div>
     )
 };
