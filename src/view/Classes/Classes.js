0cd56dddaee31f1bc4bc085f0abf825ebcf12bcb
TGDancesport <142427952+TGDancesport@users.noreply.github.com>
2023-10-22 11:26:08 -0700

    Update Classes.js

--- src/view/Classes/Classes.js
+++ src/view/Classes/Classes.js
@@ -149,7 +149,7 @@ const Classes = () => {
                     <div>Time: Wednesdays 6:30-7:25pm</div>
                     <div>Location: Scottish Cultural Centre 8886 Hudson St.</div>
                     <div style={{fontWeight:500}}>Technique 101</div>
-                    <div>Date: Sept. 06, 13, 20, 27, Oct. 04, 11, 18, 25, Nov. 01, 08, 15, 22, 2023</div>
+                    <div>Date: Sept. 06, 13, 20, 27, Oct. 04, 11, 18</div>
                     <div>Time: Wednesdays 7:30-8:25pm</div>
                     <div>Location: Scottish Cultural Centre 8886 Hudson St.</div>
                     <div style={{fontWeight:500}}>Tuition</div>
